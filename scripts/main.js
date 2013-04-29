f770277cfd1d8b77dd45a6ef55697cd5661831c1
Rafael <grafraf@mail.ru>
2013-04-29 19:04:36 +0700

    add templates

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+﻿angular.module('ngView', [], function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/templates/main.html',
+        controller: ServicesController
+    });
+
+    $routeProvider.when('/services', {
+        templateUrl: '/templates/services.html',
+        controller: ServicesController
+    });
+
+    $routeProvider.when('/services/:serviceId', {
+        templateUrl: '/templates/service.html',
+        controller: ServiceController
+    });
+
+    $routeProvider.when('/contacts', {
+        templateUrl: '/templates/contacts.html',
+        controller: ContactsController
+    });
+
+    // configure html5 to get links working on jsfiddle
+    $locationProvider.html5Mode(true);
+});
+
+function MainCntl($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+}
+
+function SerivesController($scope, $routeParams) {
+    $scope.name = "ServicesController";
+    $scope.params = $routeParams;
+}
+
+function ServiceController($scope, $routeParams) {
+    $scope.name = "ServiceController";
+    $scope.params = $routeParams;
+}
+
+function ContactsController($scope, $routeParams) {
+    $scope.name = "ContactsController";
+    $scope.params = $routeParams;
+}
